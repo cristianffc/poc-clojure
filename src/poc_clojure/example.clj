4cf706ac2ec46c7e68b035982344c1a1339599c3
Cristian Fernando Flores Castañeda <cristianffc@gmail.com>
2020-06-18 18:59:53 -0300

    :sparkles: Add new statement examples

---
+++ src/poc_clojure/example.clj
@@ -0,0 +1,17 @@
+(ns poc-clojure.example)
+
+(print "Hello world")  ; say Hi
+;;Multiple lines of comment
+
+(str "Clo"  "jure") ; Returns "Clojure
+(str "Hello, " " " "world!")
+(str 3 " 2 " " hey ")
+
+(count "Hello world")
+(count "Hello")
+(count "")
+
+(println true)
+(println false)
+
+(print "Hello world" nil)  ; say Hi
